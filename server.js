17fcd2d8d5177b033dcf097c67e7b7d76849d15b
Tuomas <tuomas@internetmadecoder.com>
2024-11-08 12:11:06 +0400

    a

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { OAuthStrategy, createClient } from "@wix/sdk";
+import { orders } from "@wix/pricing-plans";
+const app = express();
+
+const client = createClient({
+    auth: OAuthStrategy({
+        clientId: `52e4ff0a-299a-4fe1-94b3-ec9212364b7d`,
+      }),
+  modules: { orders },
+});
+
+client.orders.onOrderCreated((event) => {
+  console.log(`onOrderCreated event received with data:`, event);
+  //
+  // Handle your event here
+  //
+});
+
+app.post("/webhook", express.text(), async (request, response) => {
+  try {
+    await client.webhooks.process(request.body);
+  } catch (err) {
+    console.error(err);
+    response
+      .status(500)
+      .send(`Webhook error: ${err instanceof Error ? err.message : err}`);
+    return;
+  }
+
+  response.status(200).send();
+});
+
+app.listen(3000, () => console.log("Server started on port 3000"));
